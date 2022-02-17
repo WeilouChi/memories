5b72c3dd5127718957e5926d8dafeb3070d66d93
WeilouChi <pooh0983173211@gmail.com>
2022-02-17 16:50:48 +0800

    connecting to mongodb

--- server/index.js
+++ server/index.js
@@ -7,3 +7,11 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://willow_js1:Jj19970118@cluster0.ye9zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
