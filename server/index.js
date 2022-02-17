1ea946531cea2595164c69a501dea7d2e1025d08
WeilouChi <pooh0983173211@gmail.com>
2022-02-17 18:07:04 +0800

    routing

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://willow_js1:Jj19970118@cluster0.ye9zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
