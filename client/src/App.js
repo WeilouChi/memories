dccde9d7d1ec5eaeac1933924e7260f6c72848fb
WeilouChi <pooh0983173211@gmail.com>
2022-02-18 23:11:02 +0800

    Form

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     }, [dispatch]);
 
     return (
-        <Container maxWidth="sm">
+        <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
